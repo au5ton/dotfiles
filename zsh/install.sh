0de459fe3b920e56ef470d703554fe4900d84f2c
Austin Jackson <austinjckson@gmail.com>
2019-01-08 00:48:01 -0600

    up

---
+++ zsh/install.sh
@@ -0,0 +1,6 @@
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
